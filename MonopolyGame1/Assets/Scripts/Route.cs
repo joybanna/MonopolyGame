9ac7168e2d71f40c9cf72d8e02c2937583a7c8d6
joybanna <joybanna@gmial.com>
2020-10-29 17:09:42 +0700

    Fix ignore and add base scripts Route&Stone

---
+++ MonopolyGame1/Assets/Scripts/Route.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+    private Transform[] childObjects;
+    public List<Transform> childNodeLists = new List<Transform>();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        FillNode();
+        for (int i = 0; i < childNodeLists.Count; i++)
+        {
+            Vector3 currentPos = childNodeLists[i].position;
+            if (i > 0)
+            {
+                Vector3 prePos = childNodeLists[i - 1].position;
+                Gizmos.DrawLine(prePos, currentPos);
+            }
+        }
+
+    }
+
+    private void FillNode()
+    {
+        childNodeLists.Clear();
+        childObjects = GetComponentsInChildren<Transform>();
+        foreach (Transform child in childObjects)
+        {
+            if (child != this.transform)
+            {
+                childNodeLists.Add(child);
+            }
+        }
+    }
+}
---
+++ MonopolyGame1/Assets/Scripts/Stone.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    public Route currentRoute;
+    private int routePosition;
+    private int steps;
+    private bool isMoving;
+
+    public void MoveSteps(int _steps)
+    {
+        steps = _steps;
+        StartCoroutine(Move());
+    }
+    private IEnumerator Move()
+    {
+        if (isMoving)
+        {
+            yield break;
+        }
+        isMoving = true;
+        while (steps > 0)
+        {
+            routePosition++;
+            routePosition %= currentRoute.childNodeLists.Count;
+
+            Vector3 nextPos = currentRoute.childNodeLists[routePosition].position;
+            transform.LookAt(nextPos);
+            while (MoveToNextNode(nextPos))
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.1f);
+            steps--;
+            transform.LookAt(nextPos);
+        }
+        isMoving = false;
+
+
+    }
+
+    private bool MoveToNextNode(Vector3 goal)
+    {
+        return goal != (transform.position = Vector3.MoveTowards(transform.position, goal, 8f * Time.deltaTime));
+    }
+
+}
