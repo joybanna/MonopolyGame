6c8b8cb76046cf82efe7213ac03cc08d45a26c52
joybanna <joybanna@gmial.com>
2020-10-29 17:45:29 +0700

    add RandomStepr&DetectNode

---
+++ MonopolyGame1/Assets/Scripts/DetectNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectNode : MonoBehaviour
+{
+    public Transform detector;
+    public float length;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(detector.position, new Vector3(detector.position.x, detector.position.y - length, detector.position.z));
+
+    }
+    public void Detect()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(detector.position, detector.TransformDirection(Vector3.down), out hit, length))
+        {
+            Debug.Log("hit : " + hit.collider.gameObject.name);
+        }
+    }
+
+}
---
+++ MonopolyGame1/Assets/Scripts/RandomSteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSteps : MonoBehaviour
+{
+    public int min, max;
+    public Stone playerStone;
+    [SerializeField] private int stepsRandom;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            stepsRandom = Random.Range(min, max);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerStone.MoveSteps(stepsRandom);
+        }
+    }
+
+}
